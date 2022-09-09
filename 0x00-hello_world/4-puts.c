e702c79ab284b05b97d1e35f76b13ed9f373d90b
Peculiar Erhisohwode <75625011+PeculiarE@users.noreply.github.com>
2022-09-09 05:29:01 +0100

    Fixed more linting errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)                                                                                                                                  
+int main(void)
 {
-  char Mystr[] = "\"Programming is like building a multilingual puzzle";
-  puts(Mystr);
-  return (0);
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+return (0);
 }
