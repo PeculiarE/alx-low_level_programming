82a577cb8894b938606102d0767a38cd472a07d9
PeculiarE <perhisohwode@gmail.com>
2022-09-13 21:51:09 -0700

    Created a function to sum integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * Return: Sum of the two integers
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return result;
+}
