ea45bab887350c4348133f5ed57d9ff187a05e9d
PeculiarE <perhisohwode@gmail.com>
2022-09-27 21:42:57 -0700

    More time

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of ,
+ * the memory.
+ * @s: a pointer to the memory area to fill.
+ * @b: the character to fill the memory.
+ * @n: the number of bytes to be filled.
+ *
+ * Return: a pointer to dest.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of ,
+ * the memory.
+ * @s: a pointer to the memory area to fill.
+ * @b: the character to fill the memory area.
+ * @n: the number of bytes to be filled.
+ *
+ * Return: a pointer to be filled with memory.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
+}
